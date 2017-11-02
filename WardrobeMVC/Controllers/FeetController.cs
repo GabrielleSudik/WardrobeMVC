254495e0a4d4151970f295a662e936957d8d2030
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-11-02 15:48:03 -0400

    first commit. tables, model, conts, views done. basic only.

---
+++ WardrobeMVC/Controllers/FeetController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WardrobeMVC.Models;
+
+namespace WardrobeMVC.Controllers
+{
+    public class FeetController : Controller
+    {
+        private WardrobeMVCContext db = new WardrobeMVCContext();
+
+        // GET: Feet
+        public ActionResult Index()
+        {
+            return View(db.Feet.ToList());
+        }
+
+        // GET: Feet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Foot foot = db.Feet.Find(id);
+            if (foot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foot);
+        }
+
+        // GET: Feet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Feet/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FeetID,ShoeName,Photo,Type,Color,Style,Occasion")] Foot foot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Feet.Add(foot);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(foot);
+        }
+
+        // GET: Feet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Foot foot = db.Feet.Find(id);
+            if (foot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foot);
+        }
+
+        // POST: Feet/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FeetID,ShoeName,Photo,Type,Color,Style,Occasion")] Foot foot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(foot).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(foot);
+        }
+
+        // GET: Feet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Foot foot = db.Feet.Find(id);
+            if (foot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(foot);
+        }
+
+        // POST: Feet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Foot foot = db.Feet.Find(id);
+            db.Feet.Remove(foot);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WardrobeMVC/Controllers/RecommendationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WardrobeMVC.Models;
+
+namespace WardrobeMVC.Controllers
+{
+    public class RecommendationsController : Controller
+    {
+        private WardrobeMVCContext db = new WardrobeMVCContext();
+
+        // GET: Recommendations
+        public ActionResult Index()
+        {
+            var recommendations = db.Recommendations.Include(r => r.Accessory).Include(r => r.Foot);
+            return View(recommendations.ToList());
+        }
+
+        // GET: Recommendations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recommendation recommendation = db.Recommendations.Find(id);
+            if (recommendation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recommendation);
+        }
+
+        // GET: Recommendations/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "AccessoryName");
+            ViewBag.FeetID = new SelectList(db.Feet, "FeetID", "ShoeName");
+            return View();
+        }
+
+        // POST: Recommendations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RecommendationID,FeetID,AccessoryID")] Recommendation recommendation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Recommendations.Add(recommendation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "AccessoryName", recommendation.AccessoryID);
+            ViewBag.FeetID = new SelectList(db.Feet, "FeetID", "ShoeName", recommendation.FeetID);
+            return View(recommendation);
+        }
+
+        // GET: Recommendations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recommendation recommendation = db.Recommendations.Find(id);
+            if (recommendation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "AccessoryName", recommendation.AccessoryID);
+            ViewBag.FeetID = new SelectList(db.Feet, "FeetID", "ShoeName", recommendation.FeetID);
+            return View(recommendation);
+        }
+
+        // POST: Recommendations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RecommendationID,FeetID,AccessoryID")] Recommendation recommendation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recommendation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccessoryID = new SelectList(db.Accessories, "AccessoryID", "AccessoryName", recommendation.AccessoryID);
+            ViewBag.FeetID = new SelectList(db.Feet, "FeetID", "ShoeName", recommendation.FeetID);
+            return View(recommendation);
+        }
+
+        // GET: Recommendations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recommendation recommendation = db.Recommendations.Find(id);
+            if (recommendation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recommendation);
+        }
+
+        // POST: Recommendations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recommendation recommendation = db.Recommendations.Find(id);
+            db.Recommendations.Remove(recommendation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
